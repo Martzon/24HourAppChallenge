6868d4f3109e73916bc781bffa07faf467e6e277
Martzon Muñoz <132651378+mdmunoz-act@users.noreply.github.com>
2024-04-21 11:18:42 +0800

    add needed package and modifi gitignore

--- ChallengeApp/ChallengeApp.Application/obj/Debug/net8.0/ChallengeApp.Application.AssemblyInfo.cs
+++ ChallengeApp/ChallengeApp.Application/obj/Debug/net8.0/ChallengeApp.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChallengeApp.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6700e079c9faf18e78e6b4c33b46036601d009ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChallengeApp.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChallengeApp.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
